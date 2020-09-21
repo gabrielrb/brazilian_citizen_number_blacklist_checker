41ba8a40137a28865f105075b14ac9fbc5a78238
gabriel belo <gabrielrbelo@gmail.com>
2020-09-21 18:46:11 -0300

    search form in progress

--- app/controllers/citizens_numbers_controller.rb
+++ app/controllers/citizens_numbers_controller.rb
@@ -1,2 +1,18 @@
 class CitizensNumbersController < ApplicationController
+  # before_action :set_citizen_number, only: :show
+
+  def index
+    @citizens_numbers = CitizenNumber.all
+    @search = params["search"]
+    if @search.present?
+      @number = @search["number"]
+      @citizen_number = CitizenNumber.where(number: @number).last
+    end
+  end
+
+  # private
+
+  # def set_citizen_number
+  #   @citizen_number = CitizenNumber.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'citizens_numbers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :citizens_numbers, only: [:show, :create, :destroy]
+  resources :citizens_numbers, only: :create
+  # get "citizens_numbers/:id", to: "citizens_numbers#show", as: :citizen_number
+  delete "citizens_numbers/:id", to: "citizens_numbers#destroy"
 end
