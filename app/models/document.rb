6ccfbb6747e8fe7ab8e7e8d0c4a005a2f17b1fda
gabriel belo <gabrielrbelo@gmail.com>
2020-09-22 16:31:04 -0300

    document validations created

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,6 @@
 class Document < ApplicationRecord
+  validates :number, presence: true
+  validates :number, uniqueness: true
+  validates :number, length: { is: 11 }
+  validates :number, numericality: { only_integer: true }
 end
