6a4cdc2f7ad3430934d6c245c40f26b95d75ad7c
gabriel belo <gabrielrbelo@gmail.com>
2020-09-22 12:43:10 -0300

    simple search and index created

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,11 +1,15 @@
 class DocumentsController < ApplicationController
 
-  def show
+  def index
     @documents = Document.all
     @search = params["search"]
     if @search.present?
       @number = @search["number"]
       @document = Document.where(number: @number).last
+      if @document.nil?
+        return @document_number = @number
+      end
+      @document_number = @document.number
     end
   end
 end
