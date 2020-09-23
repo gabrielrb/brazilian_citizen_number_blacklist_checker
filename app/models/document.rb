bedd32b7d40274be95b94e541144f05b196d8b45
gabriel belo <gabrielrbelo@gmail.com>
2020-09-23 10:47:28 -0300

    validations messages created in the model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ApplicationRecord
-  validates :number, presence: true
-  validates :number, uniqueness: true
-  validates :number, length: { is: 11 }
-  validates :number, numericality: { only_integer: true }
+  validates :number, presence: { message: "CPF cannot be blank." }
+  validates :number, uniqueness: { message: "CPF already blocked" }
+  validates :number, length: { is: 11, message: "CPF must have 11 digits." }
+  validates :number, numericality: { only_integer: true, message: "Type only numbers." }
 end
