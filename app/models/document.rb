6722bbea67281b739017402827267c08b85ef28c
gabriel belo <gabrielrbelo@gmail.com>
2020-09-23 20:20:27 -0300

    validation changed

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ApplicationRecord
   validates :number, presence: { message: "CPF cannot be blank." }
+  validates :number, uniqueness: { message: "CPF already blocked" }, on: :create
   validates :number, length: { is: 11, message: "CPF must have 11 digits." }
   validates :number, format: { with: /^(\d{3}[-\.]?){3}\d{2}$/, message: "CPF must have only numbers" }
 end
